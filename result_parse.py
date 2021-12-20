a56f5d568fce2eecb81b587d9f1ba9fd1942ed81
bruce <bruce@bruces-MacBook-Pro.local>
2021-12-19 20:38:17 -0500

    1

--- result_parse.py
+++ result_parse.py
@@ -7,10 +7,14 @@ colum_list = ['domain_cond','lm','tok_mean','pmi','dcpmi']
 few_shot = ['0-shot','1-shot','2-shot','5-shot','10-shot']
 wic = {element:{} for element in few_shot}
 wsc = {element:{} for element in few_shot}
+sst = {element:{} for element in few_shot}
+agn = {element:{} for element in few_shot}
 for colum in colum_list:
     for key in wic.keys():
         wic[key][colum] = 0
         wsc[key][colum] = 0
+        sst[key][colum] = 0
+        agn[key][colum] = 0
 for file in files:
     with open('results/'+file) as f:
         content = f.readlines()
@@ -31,7 +35,7 @@ for file in files:
                             wic['10-shot'][colum_list[index]] += element/3
                         else:
                             wic['0-shot'][colum_list[index]] += element/3
-                    else:
+                    elif 'wsc' in file:
                         if '1-shot' in file:
                             wsc['1-shot'][colum_list[index]] += element/3
                         elif '2-shot' in file:
@@ -42,9 +46,34 @@ for file in files:
                             wsc['10-shot'][colum_list[index]] += element/3
                         else:
                             wsc['0-shot'][colum_list[index]] += element/3
-
+                    elif 'sst' in file:
+                        if '1-shot' in file:
+                            sst['1-shot'][colum_list[index]] += element/3
+                        elif '2-shot' in file:
+                            sst['2-shot'][colum_list[index]] += element/3
+                        elif '5-shot' in file:
+                            sst['5-shot'][colum_list[index]] += element/3
+                        elif '10-shot' in file:
+                            sst['10-shot'][colum_list[index]] += element/3
+                        else:
+                            sst['0-shot'][colum_list[index]] += element/3
+                    elif 'agn' in file:
+                        if '1-shot' in file:
+                            agn['1-shot'][colum_list[index]] += element/3
+                        elif '2-shot' in file:
+                            agn['2-shot'][colum_list[index]] += element/3
+                        elif '5-shot' in file:
+                            agn['5-shot'][colum_list[index]] += element/3
+                        elif '10-shot' in file:
+                            agn['10-shot'][colum_list[index]] += element/3
+                        else:
+                            agn['0-shot'][colum_list[index]] += element/3
 df1 = pd.DataFrame.from_dict(wic)
 df2 = pd.DataFrame.from_dict(wsc)
+df3 = pd.DataFrame.from_dict(sst)
+df4 = pd.DataFrame.from_dict(agn)
 df1.to_csv('wic.csv')
 df2.to_csv('wsc.csv')
-    
+df3.to_csv('sst.csv')
+df4.to_csv('agn.csv')
+
